9b037ef0e83244a33a8f30bb0f1aefa9731808ab
poorvitcs24 <poorvit.cs24@bmsce.ac.in>
2025-11-24 10:48:16 +0530

    DLL.c

--- DLL.c
+++ DLL.c
@@ -1,82 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Node {
     int data;
     struct Node *prev, *next;
 };
-
 struct Node *head = NULL;
-
-// ---- Create / Insert at End ----
 void create(int value) {
     struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
     newnode->data = value;
     newnode->prev = newnode->next = NULL;
-
     if (head == NULL) {
         head = newnode;
         return;
     }
-
     struct Node *temp = head;
     while (temp->next != NULL)
         temp = temp->next;
-
     temp->next = newnode;
     newnode->prev = temp;
 }
-
-// ---- Insert to Left of a Given Value ----
 void insertLeft(int target, int value) {
     struct Node *temp = head;
-
     while (temp != NULL && temp->data != target)
         temp = temp->next;
-
     if (temp == NULL) {
         printf("\nValue %d not found!", target);
         return;
     }
-
     struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
     newnode->data = value;
-
     newnode->next = temp;
     newnode->prev = temp->prev;
-
     if (temp->prev != NULL)
         temp->prev->next = newnode;
     else
-        head = newnode; // inserting before the first node
-
+        head = newnode; 
     temp->prev = newnode;
 }
-
-// ---- Delete Node by Value ----
 void deleteValue(int value) {
     struct Node *temp = head;
-
     while (temp != NULL && temp->data != value)
         temp = temp->next;
-
     if (temp == NULL) {
         printf("\nValue %d not found!", value);
         return;
     }
-
     if (temp->prev != NULL)
         temp->prev->next = temp->next;
     else
-        head = temp->next; // deleting first node
-
+        head = temp->next; 
     if (temp->next != NULL)
         temp->next->prev = temp->prev;
-
     free(temp);
 }
-
-// ---- Display List ----
 void display() {
     struct Node *temp = head;
     printf("\nList: ");
@@ -85,10 +61,8 @@ void display() {
         temp = temp->next;
     }
 }
-
 int main() {
     int choice, value, target;
-
     while (1) {
         printf("\n\n--- MENU ---");
         printf("\n1. Create/Insert at End");
@@ -98,14 +72,12 @@ int main() {
         printf("\n5. Exit");
         printf("\nEnter choice: ");
         scanf("%d", &choice);
-
         switch (choice) {
             case 1:
                 printf("Enter value: ");
                 scanf("%d", &value);
                 create(value);
                 break;
-
             case 2:
                 printf("Enter target value: ");
                 scanf("%d", &target);
@@ -113,17 +85,14 @@ int main() {
                 scanf("%d", &value);
                 insertLeft(target, value);
                 break;
-
             case 3:
                 printf("Enter value to delete: ");
                 scanf("%d", &value);
                 deleteValue(value);
                 break;
-
             case 4:
                 display();
                 break;
-
             case 5:
                 return 0;
         }
